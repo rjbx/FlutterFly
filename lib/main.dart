3bc69d5715cdc1c58da46cab688e4ff9e6c90af6
rjbx <rjbx@users.noreply.github.com>
2018-11-17 16:54:28 -0500

    feature: Expand RandomWordsState to generate infinite scrolling List
    RandomWordsState added buildSuggestions() to return ListView implemented to iterate a variable number of row elements incrementing the index and adding a Divider after every pair.
    RandomWordsState added buildRows() to return a ListTile containing a WordPair specifying the font size with TextStyle.
    RandomWordsState modified build() to return a Scaffold applying material design elements to the ListView returned by _buildSuggestions paired with an AppBar.
    MyApp modified the MaterialApp return value to replace the Scaffold set as the home parameter with that returned from RandomWords()

--- lib/main.dart
+++ lib/main.dart
@@ -7,25 +7,45 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: RandomWords();
-        ),
-      ),
+        title: 'Startup name generator',
+        home: RandomWords()
     );
   }
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    final WordPair wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(title: Text("Startup name generator")),
+      body: _buildSuggestions()
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: (BuildContext context, int i) {
+          if (i.isOdd) return Divider();
+          final int index = i ~/ 2;
+          if (index >= _suggestions.length)
+            _suggestions.addAll(generateWordPairs().take(10));
+          return _buildRow(_suggestions[index]);
+        }
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont
+      )
+    );
   }
 }
 
